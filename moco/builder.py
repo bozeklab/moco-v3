28fe994ec2217aaf3fedab63d4a471eb303bb8de
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-09-28 14:31:00 +0200

    cell vit

--- moco/builder.py
+++ moco/builder.py
@@ -155,8 +155,8 @@ class MoCo_ResNet(MoCo):
 
 class MoCo_ViT(MoCo):
     def _build_projector_and_predictor_mlps(self, dim, mlp_dim):
-        hidden_dim = self.base_encoder.head.weight.shape[1]
-        del self.base_encoder.head, self.momentum_encoder.head # remove original fc layer
+        hidden_dim = 768#self.base_encoder.head.weight.shape[1]
+        #del self.base_encoder.head, self.momentum_encoder.head # remove original fc layer
 
         # projectors
         self.base_encoder.head = self._build_mlp(3, hidden_dim, mlp_dim, dim)
