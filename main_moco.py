f7b066e953ba3809d504230ff42dcf3918937cf8
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-09-28 20:10:06 +0200

    dist

--- main_moco.py
+++ main_moco.py
@@ -91,14 +91,9 @@ def get_args_parser():
                         metavar='N', help='print frequency (default: 10)')
     parser.add_argument('--resume', default='', type=str, metavar='PATH',
                         help='path to latest checkpoint (default: none)')
+
     parser.add_argument('--world-size', default=-1, type=int,
                         help='number of nodes for distributed training')
-    parser.add_argument('--rank', default=-1, type=int,
-                        help='node rank for distributed training')
-    parser.add_argument('--dist-url', default='tcp://224.66.41.62:23456', type=str,
-                        help='url used to set up distributed training')
-    parser.add_argument('--dist-backend', default='nccl', type=str,
-                        help='distributed backend')
     parser.add_argument('--seed', default=None, type=int,
                         help='seed for initializing training. ')
     parser.add_argument('--gpu', default=None, type=int,
